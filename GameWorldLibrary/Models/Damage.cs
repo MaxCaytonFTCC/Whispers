9beb47ead54bf162fbbf1ffdfba1065bbf4bb187
Kevin McCroary <mccroark8827@student.faytechcc.edu>
2023-09-01 21:08:28 -0400

    Additional Damage object for combat

---
+++ GameWorldLibrary/Models/Damage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWorldLibrary
+{
+    public class Damage
+    {
+        #region Properties
+        #endregion
+        #region Constructors
+        #endregion
+        #region Methods
+        #endregion
+    }
+}
